ae987f4c76a369e12a769c4ae53d708acb7bce32
shoon95 <tkdgns5817@gmail.com>
2024-09-11 15:59:52 +0900

    refactor: 회원 가입 시 제약 조건 추가
    - HubManager와 CompanyDeliveryUser는 유효한 HubId가 필요

--- src/main/java/com/plj/hub/user/application/service/signup/CompanyDeliveryUserSignUp.java
+++ src/main/java/com/plj/hub/user/application/service/signup/CompanyDeliveryUserSignUp.java
@@ -2,15 +2,28 @@ package com.plj.hub.user.application.service.signup;
 
 import com.plj.hub.user.domain.model.CompanyDeliveryUser;
 import com.plj.hub.user.domain.model.User;
+import com.plj.hub.user.global.dto.ResponseDto;
+import com.plj.hub.user.infrastructure.client.HubClient;
+import com.plj.hub.user.infrastructure.client.HubClientService;
+import com.plj.hub.user.infrastructure.dto.responsedto.GetHubResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class CompanyDeliveryUserSignUp implements SignUp{
+
+    private final HubClient hubClient;
+    private final HubClientService hubClientService;
+
+
     @Override
     public User signUp(String username, String password, String slackId, UUID hubId) {
+        ResponseEntity<ResponseDto<GetHubResponseDto>> hubById = hubClient.getHubById(hubId);
+        hubClientService.verifyExistsHub(hubById);
         return CompanyDeliveryUser.signUp(username, password, slackId, hubId);
     }
 }
--- src/main/java/com/plj/hub/user/application/service/signup/HubManagerSignUp.java
+++ src/main/java/com/plj/hub/user/application/service/signup/HubManagerSignUp.java
@@ -2,15 +2,27 @@ package com.plj.hub.user.application.service.signup;
 
 import com.plj.hub.user.domain.model.HubManager;
 import com.plj.hub.user.domain.model.User;
+import com.plj.hub.user.global.dto.ResponseDto;
+import com.plj.hub.user.infrastructure.client.HubClient;
+import com.plj.hub.user.infrastructure.client.HubClientService;
+import com.plj.hub.user.infrastructure.dto.responsedto.GetHubResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class HubManagerSignUp implements SignUp{
+
+    private final HubClient hubClient;
+    private final HubClientService hubClientService;
+
     @Override
     public User signUp(String username, String password, String slackId, UUID hubId) {
+        ResponseEntity<ResponseDto<GetHubResponseDto>> hubById = hubClient.getHubById(hubId);
+        hubClientService.verifyExistsHub(hubById);
         return HubManager.signUp(username, password, slackId, hubId);
     }
 }
